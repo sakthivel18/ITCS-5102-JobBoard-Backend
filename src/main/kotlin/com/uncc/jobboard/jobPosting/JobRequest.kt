8c5057b3a52a99af593c9d08a478f14e4b0b0eec
JananiSri99 <jadanurr@uncc.edu>
2023-04-24 15:01:32 -0400

    added bookmark controller and job request

---
+++ src/main/kotlin/com/uncc/jobboard/jobPosting/JobRequest.kt
@@ -0,0 +1,19 @@
+package com.uncc.jobboard.jobPosting
+
+import org.springframework.data.annotation.CreatedBy
+import java.time.LocalDate
+
+data class JobRequest(
+        val title: String,
+        var companyName: String,
+        var description: String,
+        var location: String,
+        var experience: String,
+        var jobType: String,
+        var workMode:String,
+        var addedDate: LocalDate?,
+        var applicantsApplied: Int,
+        var createdBy: String,
+        var applicationLink: String,
+        var bookmarkedBy: List<String>
+)
